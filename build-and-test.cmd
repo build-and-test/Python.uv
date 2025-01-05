85ae53ce63415a579d986e1398860f930dc68aed
Jay Bazuzi <jay@bazuzi.com>
2025-01-05 15:38:58 -0800

    ! B fix UV version pinning

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,7 +1,6 @@
 @echo off
 setlocal
 
-set "UV_VERSION=0.5.14"
 set "UV_SYSTEM_PYTHON=0"
 set "UV_FROZEN=1"
 set "UV_INSTALL_DIR=%~dp0.uv\bin"
@@ -9,7 +8,7 @@ set "UV_PYTHON_INSTALL_DIR=%~dp0.uv\python_install"
 set "UV_NO_MODIFY_PATH=1"
 set "UV_PYTHON_PREFERENCE='only-managed'"
 
-powershell -ExecutionPolicy ByPass -Command "irm https://astral.sh/uv/install.ps1 | iex"
+powershell -ExecutionPolicy ByPass -Command "irm https://astral.sh/uv/0.5.13/install.ps1 | iex"
 
 call %UV_INSTALL_DIR%\uv venv || goto :EOF
 call %UV_INSTALL_DIR%\uv pip install --requirement requirements.dev.txt --quiet || goto :EOF
--- tests.py
+++ tests.py
@@ -1,6 +1,13 @@
+import subprocess
 import unittest
 import requests
 
 class Tests(unittest.TestCase):
     def test_example(self):
+        # Uses an installed package, to ensure that the virtual environment is set up correctly
         self.assertEqual(200, requests.codes.ok)
+
+    def test_uv_version(self):
+        # Ensure that the UV version is pinned, not latest
+        result = subprocess.run([".uv/bin/uv", "--version"], stdout=subprocess.PIPE, text=True, check=True)
+        self.assertTrue(result.stdout.startswith("uv 0.5.13"))
