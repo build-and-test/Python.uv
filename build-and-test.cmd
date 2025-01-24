6c3ab323e7c56a6e363165bbbb0c14e50f64576d
Jay Bazuzi <jay@bazuzi.com>
2025-01-24 12:48:40 -0800

    @ B fail on error
    `exit /b` or `goto :eof` doesn't propagate the failure when running in GitHub actions

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -8,8 +8,12 @@ set "UV_PYTHON_INSTALL_DIR=%~dp0.uv\python_install"
 set "UV_NO_MODIFY_PATH=1"
 set "UV_PYTHON_PREFERENCE='only-managed'"
 
-powershell -ExecutionPolicy ByPass -Command "irm https://astral.sh/uv/0.5.13/install.ps1 | iex"
+powershell -ExecutionPolicy ByPass -Command "irm https://astral.sh/uv/0.5.13/install.ps1 | iex" || goto :ERROR
 
-call %UV_INSTALL_DIR%\uv venv || goto :EOF
-call %UV_INSTALL_DIR%\uv pip install --requirement requirements.dev.txt --quiet || goto :EOF
-call %UV_INSTALL_DIR%\uv run python -m unittest discover -v || goto :EOF
+call %UV_INSTALL_DIR%\uv venv || goto :ERROR
+call %UV_INSTALL_DIR%\uv pip install --requirement requirements.dev.txt --quiet || goto :ERROR
+call %UV_INSTALL_DIR%\uv run python -m unittest discover -v || goto :ERROR
+
+
+    :ERROR
+exit /b %ERRORLEVEL%
