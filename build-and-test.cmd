a39efd379eb3f0f3add70d93b76ed0cf55fd64c1
Jay Bazuzi <jay@bazuzi.com>
2025-01-24 12:53:53 -0800

    bug: fix Windows script and the error handling that hid it (#1)
    * @ B fail on error
    
    `exit /b` or `goto :eof` doesn't propagate the failure when running in GitHub actions
    
    * ! B Fix PWSH/PowerShell incompatibility
    
    * ! B fix only-managed setting syntax

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -6,10 +6,17 @@ set "UV_FROZEN=1"
 set "UV_INSTALL_DIR=%~dp0.uv\bin"
 set "UV_PYTHON_INSTALL_DIR=%~dp0.uv\python_install"
 set "UV_NO_MODIFY_PATH=1"
-set "UV_PYTHON_PREFERENCE='only-managed'"
+set "UV_PYTHON_PREFERENCE=only-managed"
 
-powershell -ExecutionPolicy ByPass -Command "irm https://astral.sh/uv/0.5.13/install.ps1 | iex"
+@REM If the this script is run by PWSH it will try to load incompatible modules
+set "PSModulePath="
 
-call %UV_INSTALL_DIR%\uv venv || goto :EOF
-call %UV_INSTALL_DIR%\uv pip install --requirement requirements.dev.txt --quiet || goto :EOF
-call %UV_INSTALL_DIR%\uv run python -m unittest discover -v || goto :EOF
+powershell -ExecutionPolicy ByPass -Command "irm https://astral.sh/uv/0.5.13/install.ps1 | iex" || goto :ERROR
+
+call %UV_INSTALL_DIR%\uv venv || goto :ERROR
+call %UV_INSTALL_DIR%\uv pip install --requirement requirements.dev.txt --quiet || goto :ERROR
+call %UV_INSTALL_DIR%\uv run python -m unittest discover -v || goto :ERROR
+
+
+    :ERROR
+exit /b %ERRORLEVEL%
