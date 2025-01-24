86221c64df495e34272fe2440aa1225c0ff3f7c1
Jay Bazuzi <jay@bazuzi.com>
2025-01-24 12:50:54 -0800

    ! B Fix PWSH/PowerShell incompatibility

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -8,6 +8,9 @@ set "UV_PYTHON_INSTALL_DIR=%~dp0.uv\python_install"
 set "UV_NO_MODIFY_PATH=1"
 set "UV_PYTHON_PREFERENCE='only-managed'"
 
+@REM If the this script is run by PWSH it will try to load incompatible modules
+set "PSModulePath="
+
 powershell -ExecutionPolicy ByPass -Command "irm https://astral.sh/uv/0.5.13/install.ps1 | iex" || goto :ERROR
 
 call %UV_INSTALL_DIR%\uv venv || goto :ERROR
